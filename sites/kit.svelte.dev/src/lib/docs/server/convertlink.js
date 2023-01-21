e5de150617e653e2607d1a9a1385109c68f03633
tomoam <29677552+tomoam@users.noreply.github.com>
2023-01-21 23:05:26 +0900

    translate 60-ada0ter-cloudflare.md into Japanese

--- sites/kit.svelte.dev/src/lib/docs/server/convertlink.js
+++ sites/kit.svelte.dev/src/lib/docs/server/convertlink.js
@@ -68,6 +68,14 @@ const docs_adapter_static = new Map([
 	['ssr をオフにする', 'Turn off ssr']
 ]);
 
+const docs_adapter_cloudflare = new Map([
+	['比較', 'Comparisons'],
+	['使い方', 'Usage'],
+	['環境変数', 'Environment variables'],
+	['トラブルシューティング', 'Troubleshooting'],
+	['ファイルシステムにアクセスする', 'Accessing the file system']
+]);
+
 // 30-advanced
 
 const docs_advanced_routing = new Map([
@@ -173,6 +181,8 @@ export function convert_link(file, heading) {
 				return docs_adapter_node.get(heading) || heading;
 			case 'docs/25-build-and-deploy/50-adapter-static.md':
 				return docs_adapter_static.get(heading) || heading;
+			case 'docs/25-build-and-deploy/60-adapter-cloudflare.md':
+				return docs_adapter_cloudflare.get(heading) || heading;
 			// 30-advanced
 			case 'docs/30-advanced/10-advanced-routing.md':
 				return docs_advanced_routing.get(heading) || heading;
